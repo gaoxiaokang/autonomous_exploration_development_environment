333bf080bc3ee76337f9fd782022dd10ea27642b
Ji Zhang <zhangji@cmu.edu>
2021-06-22 13:15:48 -0400

    update matterport branch

--- src/vehicle_simulator/src/vehicleSimulator.cpp
+++ src/vehicle_simulator/src/vehicleSimulator.cpp
@@ -343,7 +343,7 @@ int main(int argc, char** argv)
 
   ros::Publisher pubModelState = nh.advertise<gazebo_msgs::ModelState>("/gazebo/set_model_state", 5);
   gazebo_msgs::ModelState cameraState;
-  cameraState.model_name = "camera";
+  cameraState.model_name = "rgbd_camera";
   gazebo_msgs::ModelState lidarState;
   lidarState.model_name = "lidar";
   gazebo_msgs::ModelState robotState;
