884ade3b1bf69c4d794e93f9d2d7b3d2264a46cc
Ji <ji.zhang@nearearth.aero>
2022-02-21 20:34:15 -0500

    update download script

--- src/vehicle_simulator/mesh/download_environments.sh
+++ src/vehicle_simulator/mesh/download_environments.sh
@@ -10,9 +10,7 @@ ggID='1GMT8tptb3nAb87F8eFfmIgjma6Bu0reV'
 ggURL='https://drive.google.com/uc?export=download'
 
 filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
-
 html=`curl -c /tmp/gcokie -s -L "https://drive.google.com/uc?export=download&id=${ggID}"`
-
 curl -Lb /tmp/gcokie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${ggID}" -o ${filename}
 
 echo ""
